d4df476f5f5ab7cf908730195149d188982917e6
kosl90 <kos1990l@gmail.com>
2018-01-31 15:04:13 +0800

    feat: progress component.

---
+++ src/utils.tsx
@@ -0,0 +1,11 @@
+export function clamp(target: number, min: number, max: number): number {
+    if (target < min) {
+        return min;
+    }
+
+    if (target > max) {
+        return max;
+    }
+    
+    return target;
+}
